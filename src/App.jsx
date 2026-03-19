f8d5eb3149cd3bb43e6bf04829d6553d2ec4e389
Tesalriah <proella8@gmail.com>
2026-03-19 10:38:19 +0900

    할일 체크 구현

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,10 @@ function App() {
                         className="mt-3 border-2 border-gray-300 w-[85%] rounded flex items-center justify-between p-2 m-auto cursor-pointer"
                     >
                         <input type="checkbox" className="w-5 h-5" checked={todo.completed}></input>
-                        <div className="inline-block">
+                        <div
+                            className="inline-block"
+                            style={todo.completed ? { 'text-decoration': 'line-through' } : {}}
+                        >
                             {todo.content}
                             {todo.id}
                         </div>
