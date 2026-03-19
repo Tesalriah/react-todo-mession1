e9096db0012ddb4ce379038a766c307b0fd59554
Tesalriah <proella8@gmail.com>
2026-03-19 09:34:45 +0900

    할일 추가 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
+
 function App() {
+    const [todos, setTodos] = useState(['밥먹기', '공부하기', '씻기'])
+
+    const doSubmit = (e) => {
+        e.preventDefault()
+        const todo = e.target.todo.value
+        setTodos([...todos, todo])
+    }
+
     return (
         <div className="absolute top-[30%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 bg-white p-[30px_0_50px_0] text-center shadow-[0_-6px_20px_rgba(0,0,0,0.15),0_6px_20px_rgba(0,0,0,0.15)] rounded-lg w-[500px] m-auto mt-10">
             <div className="font-bold text-[4rem] bg-gradient-to-b from-blue-500 to-purple-500 bg-clip-text text-transparent">
                 Just Do It
             </div>
-            <form className="mt-5 w-full">
+            <form className="mt-5 w-full" onSubmit={doSubmit}>
                 <input
                     type="text"
+                    name="todo"
                     className="p-2 rounded-[8px] w-[70%] text-center text-black rounded border-2 border-gray-300 "
                     placeholder="할일을 입력하세요"
+                    autoComplete="off"
                 />
                 <button
                     type="submit"
@@ -24,36 +36,21 @@ function App() {
                 </button>
             </form>
             <ul className="mt-5">
-                <li className="mt-3 border-2 border-gray-300 w-[85%] rounded flex items-center justify-between p-2 m-auto cursor-pointer">
-                    <input type="checkbox" className="w-5 h-5"></input>
-                    <div className="inline-block">밥먹기</div>
-                    <button
-                        type="submit"
-                        className="ml-2 text-2xl font-bold rounded cursor-pointer text-red-500 hover:text-red-700"
+                {todos.map((todo, index) => (
+                    <li
+                        key={index}
+                        className="mt-3 border-2 border-gray-300 w-[85%] rounded flex items-center justify-between p-2 m-auto cursor-pointer"
                     >
-                        X
-                    </button>
-                </li>
-                <li className="mt-3 border-2 border-gray-300 w-[85%] rounded flex items-center justify-between p-2 m-auto cursor-pointer">
-                    <input type="checkbox" className="w-5 h-5"></input>
-                    <div className="inline-block">공부하기</div>
-                    <button
-                        type="submit"
-                        className="ml-2 text-2xl font-bold rounded cursor-pointer text-red-500 hover:text-red-700"
-                    >
-                        X
-                    </button>
-                </li>
-                <li className="mt-3 border-2 border-gray-300 w-[85%] rounded flex items-center justify-between p-2 m-auto cursor-pointer">
-                    <input type="checkbox" className="w-5 h-5"></input>
-                    <div className="inline-block">씻기</div>
-                    <button
-                        type="submit"
-                        className="ml-2 text-2xl font-bold rounded cursor-pointer text-red-500 hover:text-red-700"
-                    >
-                        X
-                    </button>
-                </li>
+                        <input type="checkbox" className="w-5 h-5"></input>
+                        <div className="inline-block">{todo}</div>
+                        <button
+                            type="submit"
+                            className="ml-2 text-2xl font-bold rounded cursor-pointer text-red-500 hover:text-red-700"
+                        >
+                            X
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     )
